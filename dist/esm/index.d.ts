60fc123aa7d97c47f783f3a6c71eba3242305a84
m-szopinski <nerdrin@gmail.com>
2024-03-21 12:36:53 +0100

    Remove dist folder

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ICloudDocsPlugin } from './definitions';
-declare const ICloudDocs: ICloudDocsPlugin;
-export * from './definitions';
-export { ICloudDocs };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const ICloudDocs = registerPlugin('ICloudDocs', {
-    web: () => import('./web').then(m => new m.ICloudDocsWeb()),
-});
-export * from './definitions';
-export { ICloudDocs };
-//# sourceMappingURL=index.js.map
